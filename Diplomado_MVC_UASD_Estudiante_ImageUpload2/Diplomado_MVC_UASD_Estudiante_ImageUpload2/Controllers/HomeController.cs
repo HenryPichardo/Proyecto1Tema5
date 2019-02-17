153df4ca54dc08d8c75c0ad8796f26a08a8bff9b
Henry Pichardo <henryfpichardog@hotmail.com>
2019-02-16 22:40:00 -0400

    Initial commit

---
+++ Diplomado_MVC_UASD_Estudiante_ImageUpload2/Diplomado_MVC_UASD_Estudiante_ImageUpload2/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Diplomado_MVC_UASD_Estudiante_ImageUpload2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult FileUpload(HttpPostedFileBase file)
+        {
+            if (file != null)
+            {
+                EstudianteDB2Entities db = new EstudianteDB2Entities();
+                string ImageName = System.IO.Path.GetFileName(file.FileName);
+                string physicalPath = Server.MapPath("~/Images/" + ImageName);
+                file.SaveAs(physicalPath);
+                tblEstudiante2 estudiante = new tblEstudiante2();
+                estudiante.Nombres = Request.Form["Nombres"];
+                estudiante.Apellidos = Request.Form["Apellidos"];
+                estudiante.Direccion = Request.Form["Direccion"];
+                estudiante.Telefono = Request.Form["Telefono"];
+                estudiante.Cedula = Request.Form["Cedula"];
+                estudiante.ImageUrl = ImageName;
+                db.tblEstudiante2.Add(estudiante);
+                db.SaveChanges();
+            }
+            return RedirectToAction("../Home/Detalle");
+
+        }
+        public ActionResult Detalle()
+        {
+            return View();
+        }
+        public ActionResult Estudiantes()
+        {
+            return View();
+        }
+    }
+}
---
+++ Diplomado_MVC_UASD_Estudiante_ImageUpload2/Diplomado_MVC_UASD_Estudiante_ImageUpload2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Diplomado_MVC_UASD_Estudiante_ImageUpload2.MvcApplication" Language="C#" %>
